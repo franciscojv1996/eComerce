edb3501e59fdf694dd6319e52bcf6bda40c07fa3
franciscojv1996 <franciscovasquez4000@gmail.com>
2024-01-05 16:30:37 -0400

    filtro de cara de imagen listo

--- src/controller/produc.controller.ts
+++ src/controller/produc.controller.ts
@@ -1,33 +1,35 @@
 import Controller from ".";
 import { catchError } from "../middleware/catchError";
 import { UploadedFile } from "express-fileupload";
-import Produc from "./../model/Produc"
+import Produc from "./../model/Produc";
 
+class ProducController extends Controller {
+  constructor() {
+    super(Produc);
+  }
 
- class ProducController extends Controller{
-    constructor(){
-        super(Produc)
-    }
+  public img() {
+    return catchError(async (req, res) => {
+      if (req.files) {
+        const file = req.files.img as UploadedFile;
 
-    public img() {
-        return catchError(async (req, res) =>{
-            if(req.files){
-                const file = req.files.img as UploadedFile;
+        if (file.mimetype == `image/png` || file.mimetype == `image/jpeg`) {
 
-                if(file.mimetype !== `image/png`){
-                    console.log("hola imagen")    
-                }
-                
-                file.mv(`./uploads/${file.name}`, err => {
-                    if(err)return res.status(500).send({ message : err })
-                })
+            //guardarimagen
+          
+          
+            console.log("hola imagen");
 
+          file.mv(`./uploads/${file.name}`, (err) => {
+            if (err) return res.status(500).send({ message: err });
+          });
 
-                console.log(file)
-            }
-        })
-    }
+        }
 
+        console.log(file);
+      }
+    });
+  }
 }
 /*
 
